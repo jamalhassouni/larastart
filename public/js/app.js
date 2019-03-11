414927b8bf7e0f345e542e243be13db4c7850631
Jamal Hassouni <jamalhassouni13@gmail.com>
2019-03-11 22:39:49 +0100

    feat:Detect Active Menu in Vue Router and Laravel Plus HTML5 history Mode

--- public/js/app.js
+++ public/js/app.js
@@ -51801,6 +51801,7 @@ var routes = [{
   component: __webpack_require__(/*! ./components/Profile.vue */ "./resources/js/components/Profile.vue").default
 }];
 var router = new vue_router__WEBPACK_IMPORTED_MODULE_0__["default"]({
+  mode: "history",
   routes: routes // short for `routes: routes`
 
 });
--- resources/js/app.js
+++ resources/js/app.js
@@ -17,6 +17,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
+    mode:"history",
     routes // short for `routes: routes`
 })
 /**
